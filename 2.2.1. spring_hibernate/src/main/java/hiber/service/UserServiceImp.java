025d9574b673dd7a8bbd88ff0e3a005ad47fb4c0
Iamstudy <plekhov.a.dm@gmail.com>
2022-05-25 21:08:09 +0300

    PP_2_2_1

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -27,8 +27,8 @@ public class UserServiceImp implements UserService {
    }
    @Transactional(readOnly = true)
    @Override
-   public Object getUserByCar(String model, int series) {
-      return userDao.getUserByCar(model, series);
+   public User getUserByCar(String model, int series) {
+      return (User) userDao.getUserByCar(model, series);
    }
 
 }
